ad55f83adea85ed1460224a20e098feb9cb4ae2b
Andy Van Bellinghen <30909307+Vanbella@users.noreply.github.com>
2018-07-24 13:03:58 -0400

    Initial script

---
+++ Tenable Nessus Version.sh
@@ -0,0 +1,12 @@
+#ExAtt Script to check versioning of Nessus
+
+#!/bin/sh
+# Check to see if Nessus Agent is installed
+NessusAgentInstalled="$(ls /Library/NessusAgent/run/sbin/ | grep nessuscli)"
+if [ "$NessusAgentInstalled" != "nessuscli" ] 
+then
+ echo "<result>N/A</result>"
+else 
+ NessusAgentVersion="$(/Library/NessusAgent/run/sbin/nessuscli -v | awk 'NR==1{print $3 " " $4 " " $5}')"
+ echo "<result>$NessusAgentVersion</result>"
+fi
