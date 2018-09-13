d447d9a6e2307c1da556295e89565531b681031f
Andy Van Bellinghen <30909307+Vanbella@users.noreply.github.com>
2018-09-13 16:28:01 -0400

    Create itrix_receiver.uninstall.sh

---
+++ itrix_receiver.uninstall.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+#  gov.gsa.citrix_receiver.uninstall.sh
+#  govgsagit
+#
+#  Created by AVB on 9/13/18.
+#  
+##############################################
+user=`who|grep console|sed -n 1p|awk '{print $1}'`
+##############################################
+#
+sudo -u $user launchctl unload /Library/LaunchAgents/com.citrix.ctxusbd.plist &>/dev/null
+#
+pkill Citrix &>/dev/null
+#
+Sleep 5
+#
+rm -rf /Applications/Citrix\ Receiver.app
+rm -rf /Library/Application\ Support/Citrix
+rm -rf /Library/Application\ Support/Citrix\ Receiver
+rm -rf /Library/Internet\ Plug-Ins/CitrixICAClientPlugIn.plugin
+rm -rf /Library/LaunchAgents/com.citrix.AuthManager_Mac.plist
+rm -rf /Library/LaunchAgents/com.citrix.ReceiverHelper.plist
+rm -rf /Library/LaunchAgents/com.citrix.ServiceRecords.plist
+rm -rf /Library/LaunchDaemons/com.citrix.ctxusbd.plist
+rm -rf /usr/local/libexec/AuthManager_Mac.app
+rm -rf /usr/local/libexec/ReceiverHelper
+rm -rf /usr/local/libexec/ServiceRecords
+rm -rf /Users/Shared/Citrix\ Receiver
+rm -rf /Users/$user/Library/Application\ Support/Citrix\ Receiver
+rm -rf /Users/$user/Library/Application\ Support/Citrix/PlugIns
+#
+echo "Citrix Receiver uninstalled" >> /var/log/GSAlog
+exit 0
