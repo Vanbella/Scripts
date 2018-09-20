bd1c65d545d1b564da14c5289ce318d7863c85e0
Andy Van Bellinghen <30909307+Vanbella@users.noreply.github.com>
2018-09-20 13:10:31 -0400

    Update citrix_receiver.uninstall.sh

--- citrix_receiver.uninstall.sh
+++ citrix_receiver.uninstall.sh
@@ -15,25 +15,25 @@ pkill Citrix &>/dev/null
 #
 Sleep 5
 #
-rm -rf /Applications/Citrix\ Receiver.app
-rm -rf /Library/Application\ Support/Citrix
-rm -rf /Library/Application\ Support/Citrix\ Receiver
-rm -rf /Library/Internet\ Plug-Ins/CitrixICAClientPlugIn.plugin
-rm -rf /Library/LaunchAgents/com.citrix.AuthManager_Mac.plist
-rm -rf /Library/LaunchAgents/com.citrix.ReceiverHelper.plist
-rm -rf /Library/LaunchAgents/com.citrix.ServiceRecords.plist
-rm -rf /Library/LaunchDaemons/com.citrix.ctxusbd.plist
-rm -rf /usr/local/libexec/AuthManager_Mac.app
-rm -rf /usr/local/libexec/ReceiverHelper
-rm -rf /usr/local/libexec/ServiceRecords
-rm -rf /Users/Shared/Citrix\ Receiver
-rm -rf /Users/$user/Library/Application\ Support/Citrix\ Receiver
-rm -rf /Users/$user/Library/Application\ Support/Citrix
-rm -rf /Users/$user/Library/Preferences/com.citrix.AuthManager.plist
-rm -rf /Users/$user/Library/Preferences/com.citrix.AuthManagerMac.plist
-rm -rf /Users/$user/Library/Preferences/com.citrix.receiver.nomas.plist
-rm -rf /Users/$user/Library/Preferences/com.citrix.ReceiverFTU.AccountRecords.plist
-rm -rf /Users/$user/Library/Preferences/com.citrix.ReceiverHelper.plist
+rm -rf /Applications/Citrix\ Receiver.app &>/dev/null
+rm -rf /Library/Application\ Support/Citrix &>/dev/null
+rm -rf /Library/Application\ Support/Citrix\ Receiver &>/dev/null
+rm -rf /Library/Internet\ Plug-Ins/CitrixICAClientPlugIn.plugin &>/dev/null
+rm -rf /Library/LaunchAgents/com.citrix.AuthManager_Mac.plist &>/dev/null
+rm -rf /Library/LaunchAgents/com.citrix.ReceiverHelper.plist &>/dev/null
+rm -rf /Library/LaunchAgents/com.citrix.ServiceRecords.plist &>/dev/null
+rm -rf /Library/LaunchDaemons/com.citrix.ctxusbd.plist &>/dev/null
+rm -rf /usr/local/libexec/AuthManager_Mac.app &>/dev/null
+rm -rf /usr/local/libexec/ReceiverHelper &>/dev/null
+rm -rf /usr/local/libexec/ServiceRecords &>/dev/null
+rm -rf /Users/Shared/Citrix\ Receiver &>/dev/null
+rm -rf /Users/$user/Library/Application\ Support/Citrix\ Receiver &>/dev/null
+rm -rf /Users/$user/Library/Application\ Support/Citrix &>/dev/null
+rm -rf /Users/$user/Library/Preferences/com.citrix.AuthManager.plist &>/dev/null
+rm -rf /Users/$user/Library/Preferences/com.citrix.AuthManagerMac.plist &>/dev/null
+rm -rf /Users/$user/Library/Preferences/com.citrix.receiver.nomas.plist &>/dev/null
+rm -rf /Users/$user/Library/Preferences/com.citrix.ReceiverFTU.AccountRecords.plist &>/dev/null
+rm -rf /Users/$user/Library/Preferences/com.citrix.ReceiverHelper.plist &>/dev/null
 #
 echo "Citrix Receiver uninstalled" >> /var/log/GSAlog
 exit 0
