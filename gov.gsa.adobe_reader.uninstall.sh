6c98a605987d41386beb0b99479d7ebd89762ffc
Andy Van Bellinghen <30909307+Vanbella@users.noreply.github.com>
2018-10-24 13:48:30 -0400

    Update gov.gsa.adobe_reader.uninstall.sh

--- gov.gsa.adobe_reader.uninstall.sh
+++ gov.gsa.adobe_reader.uninstall.sh
@@ -4,13 +4,20 @@
 #  govgsagit
 #
 #  Created by AVB on 10/23/18.
+##############################################
+user=`who|grep console|sed -n 1p|awk '{print $1}'`
+##############################################
 #
 if [ -d /Applications/Adobe\ Reader.app ]; then
 #
-killall adobereader &> /dev/null
+killall AdobeReader &> /dev/null
 rm -rf /Applications/Adobe\ Reader.app &> /dev/null
 rm -rf /Library/Application\ Support/Adobe/Reader/11.0 &> /dev/null
+rm -rf /Library/Application\ Support/Adobe/Reader/10.0 &> /dev/null
+rm -rfd /Users/$user/Library/Application\ Support/Adobe/Acrobat/11.0/ &> /dev/null
+rm -rfd /Users/$user/Library/Application\ Support/Adobe/Acrobat/10.0/ &> /dev/null
 #
+cho "Adobe Reader Uninstalled" >> /var/log/GSAlog
 else
 echo "Adobe Reader not installed" >> /var/log/GSAlog
 fi
